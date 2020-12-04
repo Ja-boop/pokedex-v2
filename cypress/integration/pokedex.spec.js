e06147e5c6e16ef0a7a4190e5d4b1281f1b8643d
Ja-boop <agusmisiones@gmail.com>
2020-12-04 16:36:01 -0300

    Fetch y fixture cypress

--- cypress/integration/pokedex.spec.js
+++ cypress/integration/pokedex.spec.js
@@ -4,23 +4,38 @@ const URL = "127.0.0.1:8080"
 
 context('Pokedex', () => {
 
-    before(() => {
-        cy.visit(URL);
-    });
-
     describe('usa la pokedex', () => {
+        let fetchPolyfill;
+        before(() => {
+            const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
+        
+            cy.request(polyfillUrl)
+              .then((response) => {
+                fetchPolyfill = response.body;
+            });
 
+            cy.visit('http://127.0.0.1:8080', {
+              onBeforeLoad(contentWindow) {
+                // eslint-disable-next-line no-param-reassign
+                delete contentWindow.fetch;
+                contentWindow.eval(fetchPolyfill);
+                // eslint-disable-next-line no-param-reassign
+                contentWindow.fetch = contentWindow.unfetch;
+              },
+            });
+          });
+        
         it('busca un pokemon', () => {
             cy.get('#barra-busqueda').type('Pikachu');
-            cy.get('#lista-resultado-pokemon').should('have.length', 1);
-            cy.contains('pikachu').click();
+            cy.get('#lista-resultado-pokemon').should('have.length', 1);  
         });
 
         it('selecciona al pokemon', () => {
-            cy.get('#lista-resultado-pokemon').click();
+            cy.contains('pikachu').click();
         });
 
         it('muestra los datos', () => {
+            cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon/pikachu', {fixture: 'pikachu.json'})
             cy.get('#imagen-pokemon').should('have.length', 1);
             cy.get('#id-pokemon').should('have.length', 1);
             cy.get('#nombre-pokemon').should('have.length', 1);
